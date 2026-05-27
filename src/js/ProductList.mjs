acaf569afb31ec779acdfde7a2ecdef42bc92766
alicemartins-hue <alicemartins7l@hotmail.com>
2026-05-27 15:02:19 -0300

    fixing bug

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,21 @@
+
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+  return `<li class="product-card">
+            <a href="product_pages/?product=${product.Id}">
+              <img
+                src="${product.Image}"
+                alt="${product.Name}"
+              />
+              <h3 class="card__brand">${product.Brand.Name}</h3>
+              <h2 class="card__name">${product.Name}</h2>
+              <p class="product-card__price">$${product.ListPrice}</p>
+            </a>
+          </li>
+        `;
+}
+
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
